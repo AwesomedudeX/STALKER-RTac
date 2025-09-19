ca5360523a8a69d4fccc99f6d54349d818539b21
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2025-09-18 20:38:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1011,7 +1011,7 @@ Along with that, you will need some other files, including **modified engine fil
 
 ***RTac GAMMA Large Mods (~2.7GB packed; ~10GB unpacked)***: https://www.mediafire.com/file/rscrhg1wbz4t9m0/RTac+GAMMA+Large+Mods.7z/file
 
-***STALKER RTac (~11GB packed; ~46GB unpacked)***: https://www.mediafire.com/file/ctetuhujux4cwau/STALKER+RTac.7z/file
+***STALKER RTac (~11GB packed; ~46GB unpacked)***: https://www.mediafire.com/file/7z18neb9nwx6l4b/STALKER+RTac.7z/file
 
 ***Arrival Anomalies - S.e.m.i.t.o.n.e.***: https://www.moddb.com/mods/stalker-anomaly/addons/arrival-anomalies
 
