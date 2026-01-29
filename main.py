a5c8442208f6dd136dc5c75ee629a46ef0bac8d5
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2026-01-28 20:08:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -775,7 +775,7 @@ I'd also recommend using it with **Anomaly Mod Configuration Menu**, which you c
 
 ***Atmospherics GAMMA (ModDB: File size changes with updates)***: https://www.moddb.com/mods/stalker-anomaly/addons/hippos-shaders-and-atmospherics
 
-***RESHADE 6.3.3 DX11 (Google Drive: ~115MB packed; ~240MB unpacked)***: https://drive.google.com/uc?export=download&id=1XiLQLt8_BdfBLbGyobAYtzebaDATjvIy
+***RESHADE 6.3.3 DX11 (Google Drive: ~115MB packed; ~240MB unpacked)***: https://drive.google.com/uc?export=download&id=1PWpfTfCQYEWr9GsKe0k5X6emepEdT-Rq
 
 If you want the full pack, head to the **STALKER RTac** page to check it out; you won't need the instructions on this page if you are using it. If you only want the graphics pack, follow the instructions here to install it.
 """,
@@ -1396,3 +1396,4 @@ elif page == "Atmospheric Preset Editor":
             st.write(f"```{preset}\n```")
 
         st.download_button(f"**Download Your Preset (:blue[{presetname}])**", data=preset, file_name=presetname)
+
